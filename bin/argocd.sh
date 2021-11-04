6cf0850206c7dcec71a23ef0805527346da095bb
Walker O'Brien <walkernobrien@gmail.com>
2021-11-04 02:09:39 -0500

    almost

--- bin/argocd.sh
+++ bin/argocd.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 helm upgrade -i platform-cd \
-  walker-charts/argocd-platform \
-  -n platform-system --create-namespace \
-  -f ./argocd/values/$LIFECYCLE.yaml --dry-run
+charts/argocd-platform \
+-n platform-system --create-namespace \
+-f ./charts/argocd-platform/values/default.yaml \
+-f ./charts/argocd-platform/values/$LIFECYCLE.yaml # --dry-run
