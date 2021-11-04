e2867820a8c17cf818048d1435056310a4d5c705
Walker O'Brien <walkernobrien@gmail.com>
2021-11-04 17:06:02 -0500

    updating

--- bin/argocd.sh
+++ bin/argocd.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 
-helm upgrade -i argocd-root \
-charts/argocd-root \
--n argocd-system --create-namespace \
+# helm template argocd-root \
+# charts/argocd-root \
+# -n argocd-system --create-namespace \
+# --set lifecycle=$LIFECYCLE
+
+
+helm template argocd-platform \
+charts/argocd-platform \
+-n platform-cd --create-namespace \
 --set lifecycle=$LIFECYCLE
