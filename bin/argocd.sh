94b05367f8f77bb47e9ae093c27b0ca418dd92c0
Walker O'Brien <walkernobrien@gmail.com>
2021-11-04 18:30:05 -0500

    updating argocd install chart

--- bin/argocd.sh
+++ bin/argocd.sh
@@ -4,7 +4,7 @@ helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm dep update charts/argocd-install/
 echo "charts/" > charts/argocd-install/.gitignore
 
-helm install argocd -n argocd --create-namespace charts/argocd-install
+helm upgrade -i argocd -n argocd --create-namespace charts/argocd-install
 
 
 
